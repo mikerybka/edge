7b8ec28c73ebbe95803b49715ead1eb01878f269
Mike Rybka <merybka@gmail.com>
2025-07-29 18:09:40 -0400

    Tidy

--- demo.ts
+++ demo.ts
@@ -1,16 +1,5 @@
-// // Generate screen image data
 const width = 640;
 const height = 480;
-// const imageData = new ImageData(width, height);
-// const data = imageData.data;
-// // Set every pixel to white (RGBA: 255,255,255,255)
-// for (let i = 0; i < data.length; i += 4) {
-//     data[i] = 255;     // Red
-//     data[i + 1] = 255; // Green
-//     data[i + 2] = 255; // Blue
-//     data[i + 3] = 255; // Alpha (opaque)
-// }
-// console.log(data.length)
 
 async function render() {
     const res = await fetch("/demo/frame");
@@ -36,8 +25,6 @@ if (!body) {
 }
 body.style.margin = '0';
 body.style.padding = '0';
-// body.style.backgroundColor = '#000';
-// body.style.color = '#fff';
 body.style.width = '100vw';
 body.style.height = '100vh';
 body.style.display = 'flex';
@@ -55,20 +42,3 @@ const socket = new WebSocket("ws://localhost:3005/demo/updates");
 socket.addEventListener("message", (e: MessageEvent<string>) => {
     render();
 });
-
-// Loop
-// (async () => {
-//     while (true) {
-//         try {
-//             const res = await fetch("/demo/next-frame");
-//             const buf = await res.arrayBuffer();
-//             const pixels = new Uint8ClampedArray(buf);
-//             const imageData = new ImageData(pixels, width, height);
-//             const canvas = document.getElementById('screen') as HTMLCanvasElement;
-//             const ctx = canvas.getContext('2d')!;
-//             ctx.putImageData(imageData, 0, 0);
-//         } catch (e) {
-//             console.log(e);
-//         }
-//     }
-// })()
